36a937df4ca19e9e5ae34bd72a613359a6f09755
woochang <hwc1156@gmail.com>
2025-03-23 16:20:43 +0900

    chore: 프로젝트 초기 세팅 및 패키지 구조 생성

---
+++ src/test/java/com/woochang/highticket/HighTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woochang.highticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
